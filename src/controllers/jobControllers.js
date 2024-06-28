cc5c6491d508b0674ba06db10c0e2eb9411dd005
marthamaxwell <marthamaxwell499@gmail.com>
2024-06-28 16:26:05 -0700

    update

--- src/controllers/jobControllers.js
+++ src/controllers/jobControllers.js
@@ -114,7 +114,7 @@ const getAllJobs = async (req, res) => {
 
     //EXECUTE QUERY
     const features = new ApiFeatures(Job.find(), req.query)
-    .filter().sort().limitFields().paginate()
+.filter().sort().limitFields().paginate()
     const job = await features.query.exec()
     console.log("QUERY:", features.query, "QUERY OBJECT:", queryObj);
     // console.log("QUERY:", req.query, "QUERY OBJECT:", queryObj);
